4411706681872473318157a02ffd916ee0f90d16
Sean Chatman <xpointsh@gmail.com>
2020-12-26 15:15:43 -0800

    First commit 12/26/20 3:15PM

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <component v-bind:is=""></component>
+  </q-page>
+</template>
+
+<script>
+import survey from "../assets/survey.json"
+export default {
+  name: 'PageIndex',
+  data(){
+    return {
+      survey
+    }
+  },
+}
+</script>
